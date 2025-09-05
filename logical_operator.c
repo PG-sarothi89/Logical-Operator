3ca65adbd23a5ce03d96df5be43b0c773755a16d
PsGhosh <pgsarothi2005@gmail.com>
2025-09-05 22:17:57 +0600

    first commit

---
+++ logical_operator.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=5, b=10, c=15;
+    if (a<b && b<c){
+        printf("Both conditions are true\n");
+    }
+    if (a>b || b<c){
+        printf("At least one condition is true\n");
+    }
+    if(!(a>b)){
+        printf("Condition is false\n");
+    }   
+    return 0;
+}
